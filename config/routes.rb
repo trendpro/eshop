b8284f460a720e0e293aa5f2be73def98cc3bc0d
dMitry <nquincy47@gmail.com>
2013-06-01 12:33:57 +0300

    Add eShop catalog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Eshop::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
 
@@ -51,7 +53,7 @@ Eshop::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
